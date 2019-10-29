1e6c008cc1cc020cb80d678317130b43930d6d22
Marco Vinícius Castro Gonçalves <mvcastro1975@gmail.com>
2019-10-29 15:00:56 -0300

    versao_final

--- massas_dagua_gee.py
+++ massas_dagua_gee.py
@@ -8,63 +8,67 @@ def retorna_lista(img, featCol):
   """Analisa cada imagem da coleção e retorna uma lista com os resultados
       sendo cada item da lista referente a uma data"""
 
-  # Retorna uma FeatureCollection com o número de pixels dentro de cada polígono de massa d'água
-  data_count = img.reduceRegions(featCol, ee.Reducer.count(), 500)
+  # Retorna uma FeatureCollection com o número de pixels dentro de cada polígono de massa d'água 
+  data_count = img.reduceRegions(featCol, ee.Reducer.count(), 30)
   # Inserção da data da imagem analisada nas propriedades da FeatureCollection
   data_count = data_count.map(lambda feat: feat.set('data_analise', img.date().format()))
-
+ 
   # Retona uma FeatureCollection com a média dos valores dos pixels inseridos
   #  em cada polígono de massa d'água
-  data_mean = img.reduceRegions(featCol, ee.Reducer.mean(), 500)
-  # Inserção da data da imagem analisada nas propriedades da FeatureCollection
-  data_mean = data_mean.map(lambda feat: feat.set('data_analise', img.date().format()))
+  data_count_mean = img.reduceRegions(data_count, ee.Reducer.mean(), 30)
 
   # Retona uma FeatureCollection com a mediana dos valores dos pixels inseridos
   #  em cada polígono de massa d'água
-  data_median = img.reduceRegions(featCol, ee.Reducer.median(), 500)
-  # Inserção da data da imagem analisada nas propriedades da FeatureCollection
-  data_median = data_median.map(lambda feat: feat.set('data_analise', img.date().format()))
+  data_count_mean_median = img.reduceRegions(data_count_mean, ee.Reducer.median(), 30)
+  
+  feats_stats = data_count_mean_median.getInfo()['features']
 
-  feats_count = data_count.getInfo()['features']
-  feats_mean = data_mean.getInfo()['features']
-  feats_median = data_median.getInfo()['features']
-
-  return [feats_count, feats_mean, feats_median]
+  return [feats_stats]
 
 
 def retorna_dataframe(result):
     # Itens a serem retirados do resultado
-    esp_cd = []
     data_analise = []
-    data_const = []
-    nooriginal = []
+    idx_pacote = []
+    vx_vacum = []
+    esp_cd = []
+    nuareaha = []
     count = []
     mean = []
     median = []
 
-    # for feat in result['features']:
+    count_flag = False
+    mean_flag = False
+    median_flag = False
+
     for feat in result:
-        esp_cd.append(feat['properties']['esp_cd'])
         data_analise.append(feat['properties']['data_analise'])
-        data_const.append(feat['properties']['data_const'])
-        nooriginal.append(feat['properties']['nooriginal'])
+        idx_pacote.append(feat['properties']['IDX_PACOTE'])
+        vx_vacum.append(feat['properties']['VXACUM'])
+        esp_cd.append(feat['properties']['intESP_CD'])
+        nuareaha.append(feat['properties']['nuareaha'])
         if 'count' in feat['properties']:
             count.append(feat['properties']['count'])
+            count_flag = True
         if 'mean' in feat['properties']:
             mean.append(feat['properties']['mean'])
+            mean_flag = True
         if 'median' in feat['properties']:
             median.append(feat['properties']['median'])
+            median_flag = True
 
+        # Criando um objeto dicionário com os resultados
     feature = {}
-    feature['esp_cd'] = esp_cd
     feature['data_analise'] = data_analise
-    feature['data_const'] = data_const
-    feature['nooriginal'] = nooriginal
-    if 'count' in feat['properties']:
+    feature['idx_pacote'] = idx_pacote
+    feature['vx_vacum'] = vx_vacum
+    feature['esp_cd'] = esp_cd
+    feature['nuareaha'] = nuareaha
+    if count_flag:
         feature['count'] = count
-    if 'mean' in feat['properties']:
+    if mean_flag:
         feature['mean'] = mean
-    if 'median' in feat['properties']:
+    if median_flag:
         feature['median'] = median
 
     return pd.DataFrame(feature, index=data_analise)
@@ -72,51 +76,56 @@ def retorna_dataframe(result):
 
 def retorna_df_merged(lista_result):
 
-    count = lista_result[0]
-    mean = lista_result[1]
-    median = lista_result[2]
-
-    df_result_count = retorna_dataframe(count)
-    df_result_mean = retorna_dataframe(mean)
-    df_result_median = retorna_dataframe(median)
-
-    df_result = df_result_count\
-        .merge(df_result_mean[['esp_cd', 'mean']], on='esp_cd')\
-    .merge(df_result_median[['esp_cd', 'median']], on='esp_cd')
-
-    return df_result
+    stats = lista_result[0]
+    
+    df_result_stats = retorna_dataframe(stats)
+    
+    return df_result_stats
 
 
 if __name__ == "__main__":
 
-  #Tempo inicial
-  timeInMsBefore = datetime.now()
+    # Tempo inicial
+    timeInMsBefore = datetime.now()
 
-  # Importação do Shapefile com massas d'água
-  massas_dagua = ee.FeatureCollection("users/mvcastro1975/massa_dagua/massa_dagua_maior_100ha")
+    # Importação do Shapefile com massas d'água
+    massas_dagua = ee.FeatureCollection("users/mvcastro1975/pol_ScriptV3_08")
+    # Lista de elementos da FeatureCollection
+    lista_massas_dagua = massas_dagua.toList(massas_dagua.size())
+		# Número de massa d'água no shapefile
+    num_massas = massas_dagua.size().getInfo()
 
-  anos = range(1984, 1985)
-  meses = range(1, 5)
+    # Processa 1000 polígonos por vez (pol_ScriptV3_08 possui 8.781 polígonos)
+    for limite in range(1, int(num_massas/1000) + 2):
 
-  # Importa dataset com recorrência mensal de água
-  monthly_hist = ee.ImageCollection('JRC/GSW1_0/MonthlyHistory').select('water')
+        # Lista com 1000 Features por vez (divide o shapefile a cada 1000 polígonos)
+        lista_parcial = lista_massas_dagua.slice(
+            (limite - 1) * 1000, limite * 1000)
+        # Criando FeatureCollection com 1000 valores
+        massas_dagua_parcial = ee.FeatureCollection(lista_parcial)
 
-  resultado = []
+        # Anos a serem considerados no processamento
+        anos = range(2002, 2003)
+        # Meses do ano
+        meses = range(1, 2)
 
-  for ano in anos:
-    for mes in meses:
-      imagem = monthly_hist.filter(ee.Filter.And(\
-                ee.Filter.eq('year', ano),\
-                ee.Filter.eq('month', mes)))
-      # Retorna resultado para a imagem na data em análise e as feições de massa d'água 
-      if imagem.size().getInfo() != 0:
-        lista_result = retorna_lista(imagem.first(), massas_dagua)
-        df_result = retorna_df_merged(lista_result)
-        #pasta onde serão armazenados os dados
-        path = 'C:/Temp/GEE/resultado{}-{}.csv'.format(ano, mes)
-        df_result.to_csv(path, sep=';')
-  
-  
-  # Tempo final
-  timeInMsAfter = datetime.now()
-  print(timeInMsAfter - timeInMsBefore)
+        # Importa dataset com detcção de água para cada mês da série
+        monthly_hist = ee.ImageCollection('JRC/GSW1_0/MonthlyHistory').select('water')
+
+        for ano in anos:
+            for mes in meses:
+                imagem = monthly_hist.filter(ee.Filter.And(\
+                    ee.Filter.eq('year', ano),\
+                    ee.Filter.eq('month', mes)))
+                # Retorna resultado para a imagem na data em análise e as feições de massa d'água
+                if imagem.size().getInfo() != 0:
+                    lista_result = retorna_lista(imagem.first(), massas_dagua_parcial)
+                    df_result = retorna_df_merged(lista_result)
+                    # pasta escolhida para armazenamento dos dados
+                    path = 'C:/Temp/GEE2/resultado_pol_ScriptV3_08_ano{}mes{}_parte{}.csv'.format(\
+                            ano, mes, limite)
+                    df_result.to_csv(path, sep=';', decimal=',')
+
+    # Tempo final
+    timeInMsAfter = datetime.now()
+    print(timeInMsAfter - timeInMsBefore)
